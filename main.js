a671acaf35423a1065005c5bbda16a2e3685cf6f
ru-lai <ru.lai.development@gmail.com>
2019-09-29 17:36:14 -0700

    modifies the main.js to use the rust_text_adventure file instead of the base template

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 import init, { run_app } from './pkg/rust_text_adventure.js';
 async function main() {
-   await init('/pkg/yew_wasm_pack_minimal_bg.wasm');
+   await init('/pkg/rust_text_adventure_bg.wasm');
    run_app();
 }
 main()
